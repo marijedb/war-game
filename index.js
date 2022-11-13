e8af0e90fb85f98a9e6274f47b95a47f2a464179
marijedb <marijedb@live.nl>
2022-11-13 22:04:01 +0100

    updated styles.css and added render functionality

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 let deckId
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardsBtn = document.getElementById("draw-cards")
+const cardsContainer = document.getElementById("cards")
+drawCardsBtn.disabled = true
+
+newDeckBtn.addEventListener("click", handleClick)
+drawCardsBtn.addEventListener("click", drawCards)
+
 
 function handleClick() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
             deckId = data.deck_id
+            drawCardsBtn.disabled = false
         })
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+function drawCards(){
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(response => response.json())
+        .then(data => {
+            cardsContainer.children[0].innerHTML = `
+            <img src=${data.cards[0].image} class="card" alt='${data.cards[0].value} of ${data.cards[0].suit}'>
+            `
+            cardsContainer.children[1].innerHTML = `
+            <img src=${data.cards[1].image} class="card" alt='${data.cards[1].value} of ${data.cards[1].suit}'>
+            `
+        })
+}
